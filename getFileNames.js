46148a5a42b40f69c6dc396c59eda9012b18c340
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2020-04-09 13:51:09 +0200

    Dodanie skryptu tworzącego plik ze zmienna fileNames - wszystkie pliki z folderu assets.

---
+++ getFileNames.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var files = fs.readdirSync('./assets/');
+var outputArray = []
+for(var i = 0; i < files.length; i++) {
+    outputArray.push(files[i])
+}
+
+fs.writeFile("./namesArray.txt", "var fileNames = " + JSON.stringify(outputArray), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
